2c45ae99bf2d7a6020d6fc9c584a90ff713541a1
Oppedal <nicholai.oj@gmail.com>
2022-05-13 00:24:38 +0200

    some refactoring

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ let temp;
 let screenNumber;
 let numbers = [];
 let total;
+let finished = false;
 const reset = () => {
   num1 = null;
   num2 = null;
@@ -40,78 +41,88 @@ const operate = (num1, num2) => {
   }
   return result;
 };
+const calc = () => {
+  if (num1 === null) {
+    num1 = screenNumber;
+  } else if (num1 !== null) {
+    num2 = screenNumber;
+    let sum = operate(num1, num2);
+    num1 = sum;
+    screen.textContent = num1;
+  }
+  numbers = [];
+  screenNumber = null;
+};
 
-document.querySelector('.number1').addEventListener('click', () => {
+document.querySelector('.number--1').addEventListener('click', () => {
   numbers.push(1);
   screenNumber = Number(numbers.join(''));
   screen.textContent = screenNumber;
 });
-document.querySelector('.number2').addEventListener('click', () => {
+document.querySelector('.number--2').addEventListener('click', () => {
   numbers.push(2);
+  screenNumber = Number(numbers.join(''));
+  screen.textContent = screenNumber;
+});
+document.querySelector('.number--3').addEventListener('click', () => {
+  numbers.push(3);
+  screenNumber = Number(numbers.join(''));
+  screen.textContent = screenNumber;
+});
+document.querySelector('.number--4').addEventListener('click', () => {
+  numbers.push(4);
+  screenNumber = Number(numbers.join(''));
+  screen.textContent = screenNumber;
+});
+document.querySelector('.number--5').addEventListener('click', () => {
+  numbers.push(5);
+  screenNumber = Number(numbers.join(''));
+  screen.textContent = screenNumber;
+});
+document.querySelector('.number--6').addEventListener('click', () => {
+  numbers.push(6);
+  screenNumber = Number(numbers.join(''));
+  screen.textContent = screenNumber;
+});
+document.querySelector('.number--7').addEventListener('click', () => {
+  numbers.push(7);
+  screenNumber = Number(numbers.join(''));
+  screen.textContent = screenNumber;
+});
+document.querySelector('.number--8').addEventListener('click', () => {
+  numbers.push(8);
+  screenNumber = Number(numbers.join(''));
+  screen.textContent = screenNumber;
+});
+document.querySelector('.number--9').addEventListener('click', () => {
+  numbers.push(9);
   screenNumber = Number(numbers.join(''));
   screen.textContent = screenNumber;
 });
 
 document.querySelector('.add').addEventListener('click', () => {
   temp = add;
-  if (num1 === null) {
-    num1 = screenNumber;
-  } else if (num1 !== null) {
-    num2 = screenNumber;
-    let sum = operate(num1, num2);
-    num1 = sum;
-    screen.textContent = num1;
-  }
-  numbers = [];
-  screenNumber = null;
+  finished ?? reset();
+  calc();
 });
 document.querySelector('.subtract').addEventListener('click', () => {
   temp = subtract;
-  if (num1 === null) {
-    num1 = screenNumber;
-  } else if (num1 !== null) {
-    num2 = screenNumber;
-    let sum = operate(num1, num2);
-    num1 = sum;
-    screen.textContent = num1;
-  }
-  numbers = [];
-  screenNumber = null;
+  finished ?? reset();
+  calc();
 });
 document.querySelector('.multiply').addEventListener('click', () => {
   temp = multiply;
-  if (num1 === null) {
-    num1 = screenNumber;
-  } else if (num1 !== null) {
-    num2 = screenNumber;
-    let sum = operate(num1, num2);
-    num1 = sum;
-    screen.textContent = num1;
-    if (num1 === total) {
-      num2 = null;
-    }
-  }
-  numbers = [];
-  screenNumber = null;
+  finished ?? reset();
+  calc();
 });
 document.querySelector('.divide').addEventListener('click', () => {
   temp = divide;
-  if (num1 === null) {
-    num1 = screenNumber;
-  } else if (num1 !== null) {
-    num2 = screenNumber;
-    let sum = operate(num1, num2);
-    num1 = sum;
-    screen.textContent = num1;
-    if (num1 === total) {
-      num2 = null;
-    }
-  }
-  numbers = [];
-  screenNumber = null;
+  finished ?? reset();
+  calc();
 });
 
 document.querySelector('.equals').addEventListener('click', () => {
+  finished = true;
   num2 = screenNumber;
   total = operate(num1, num2);
   screen.textContent = total;
