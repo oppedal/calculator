afc75be5d157af70337fc4ddd8f766ee6819bcf0
Oppedal <nicholai.oj@gmail.com>
2022-05-12 14:34:35 +0200

    Started

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+let num1 = 2;
+let num2 = 3;
+let temp = multiply;
+const operate = (num1, num2) => {
+  let result;
+  switch (temp) {
+    case add:
+      result = add(num1, num2);
+      break;
+    case subtract:
+      result = subtract(num1, num2);
+      break;
+    case multiply:
+      result = multiply(num1, num2);
+      break;
+  }
+  return result;
+};
+
+console.log(operate(3, 2));
